f2e7ad5a008affa2dbb0e5373a95fe282cb5bd2a
moniquests <moniquests@gmail.com>
2017-11-28 23:36:56 +0000

    github

--- app/models/aeronafe.rb
+++ app/models/aeronafe.rb
@@ -1,2 +1,10 @@
 class Aeronafe < ActiveRecord::Base
+
+validates_presence_of :nome, on: :create, message: " não pode ficar em branco"
+validates_presence_of :numeroTripulantes, on: :create, message: " não pode ficar em branco"
+validates_presence_of :tamanho, on: :create, message: " não pode ficar em branco"
+validates_presence_of :numero, on: :create, message: " não pode ficar em branco"
+validates_presence_of :chassi, on: :create, message: " não pode ficar em branco"
+validates_presence_of :quantidadeHorasVoo, on: :create, message: " não pode ficar em branco"
+validates_presence_of :quantidadeMotor, on: :create, message: " não pode ficar em branco"
 end
--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,2 +1,12 @@
 class Aluno < ActiveRecord::Base
+validates_presence_of :nome, on: :create, message: " não pode ficar em branco"
+validates_presence_of :sobrenome, on: :create, message: " não pode ficar em branco"
+validates_presence_of :cpf, on: :create, message: " não pode ficar em branco"
+validates_presence_of :endereco, on: :create, message: " não pode ficar em branco"
+validates_presence_of :profissao, on: :create, message: " não pode ficar em branco"
+validates_presence_of :email, on: :create, message: " não pode ficar em branco"
+validates_presence_of :cnh, on: :create, message: " não pode ficar em branco"
+validates_presence_of :escolaridade, on: :create, message: " não pode ficar em branco"
+validates_presence_of :instituicao, on: :create, message: " não pode ficar em branco"
+validates_presence_of :curso, on: :create, message: " não pode ficar em branco"
 end
